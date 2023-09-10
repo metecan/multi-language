e5825fdadc55a0fbebe1ecbefb6a084296bc7a7b
metecan <metecankaplan@hotmail.com>
2023-09-10 17:02:38 +0300

    [FEAT]: dummy data created

---
+++ src/data/languageOptions.ts
@@ -0,0 +1,12 @@
+export const LanguageOptions = [
+  {
+    label: 'TUR',
+    value: 'turkish',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/1280px-Flag_of_Turkey.svg.png',
+  },
+  {
+    label: 'ENG',
+    value: 'english',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/1920px-Flag_of_the_United_Kingdom_%281-2%29.svg.png',
+  },
+];
