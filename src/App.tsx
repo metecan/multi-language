7358a875fb7d9376005d32cab843fd0ae54164c4
metecan <metecankaplan@hotmail.com>
2023-09-10 17:02:51 +0300

    [UPDATE]: import update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar';
 import Pricing from './pages/Pricing';
 
 interface AppProps {}
