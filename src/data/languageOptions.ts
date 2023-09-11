bd6ab5ef45ebc517a5d2fbed094d0dd69c8d891a
metecan <metecankaplan@hotmail.com>
2023-09-11 09:37:35 +0300

    [FEAT]: multi language files created

--- src/data/languageOptions.ts
+++ src/data/languageOptions.ts
@@ -1,12 +1,12 @@
 export const LanguageOptions = [
   {
     label: 'TUR',
-    value: 'turkish',
+    value: 'tr',
     icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/1280px-Flag_of_Turkey.svg.png',
   },
   {
     label: 'ENG',
-    value: 'english',
+    value: 'en',
     icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/1920px-Flag_of_the_United_Kingdom_%281-2%29.svg.png',
   },
 ];
